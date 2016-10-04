cd55601a4362a26c579b9f39e456e238c225b851
Chris Nuernberger <chris@thinktopic.com>
2016-10-04 17:21:08 -0600

    hopefully last small piece

--- test-deploy-docker.sh
+++ test-deploy-docker.sh
@@ -2,4 +2,5 @@
 docker run -t --name testmg -e VAULT_ADDR=$VAULT_ADDR -e VAULT_TOKEN=$VAULT_TOKEN -e LEIN_ROOT=t \
        -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
        -e LEIN_USERNAME=$AWS_ACCESS_KEY_ID -e LEIN_PASSWORD=$AWS_SECRET_ACCESS_KEY -e LEIN_PASSPHRASE=$AWS_SECRET_ACCESS_KEY \
-       --volume=`pwd`:/root/build hdf5-buildimg /bin/bash -c "cd /root/build && lein test && lein deploy"
+       --volume=`pwd`:/root/build hdf5-buildimg \
+       /bin/bash -c "cd /root/build && ./build-jni.sh && lein test && lein deploy"
