94eaa87f74c19e1448ebcbfb65d47d8dcd73f1ae
Chris Nuernberger <chris@thinktopic.com>
2016-10-04 13:25:20 -0600

    Enabling travis builds.

--- test/think/hdf5/core_test.clj
+++ test/think/hdf5/core_test.clj
@@ -1,7 +1,11 @@
 (ns think.hdf5.core-test
   (:require [clojure.test :refer :all]
-            [think.hdf5.core :refer :all]))
+            [think.hdf5.core :refer :all]
+            [resource.core :as resource]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest basic-test
+  (resource/with-resource-context
+    (let [test-file (open-file "test_data/h5ex_g_create.h5")
+          test-data (mapv ->clj (get-children test-file))]
+      (is (= test-data [{:attributes [] :children [] :name "G1" :type :group}])))))
