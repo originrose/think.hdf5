71b6f58373ccbc78bfb82e5a4ca6d6055a3eff68
Chris Nuernberger <chris@thinktopic.com>
2016-11-08 10:15:17 -0700

    Deploying 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/hdf5 "0.1.1"
+(defproject thinktopic/hdf5 "0.1.2"
   :description "Small jni wrapper to read hdf5 files."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
