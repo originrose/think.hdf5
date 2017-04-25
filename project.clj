65920a6edd57ca60eeb5f1e5733731afb4a3f7c1
Chris Nuernberger <chris@thinktopic.com>
2017-04-25 15:23:02 -0600

    Release 0.1.3

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject thinktopic/hdf5 "0.1.3-SNAPSHOT"
+(defproject thinktopic/hdf5 "0.1.3"
   :description "Small jni wrapper to read hdf5 files."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.bytedeco/javacpp "1.2.4"]
-                 [thinktopic/resource "1.1.0"]]
+                 [thinktopic/think.resource "1.2.1"]]
 
   :java-source-paths ["java"]
   :native-path "java/native/"
